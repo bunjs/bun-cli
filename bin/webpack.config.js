46ea92edcdf587782b95ceb97cedb060b356594c
Li <liguohui01@baidu.com>
2018-07-11 21:24:37 +0800

    add webpack-config.js

---
+++ bin/webpack.config.js
@@ -0,0 +1,218 @@
+module.exports = (userConf) => {
+    const webpack = require('webpack');
+    const CopyWebpackPlugin = require('copy-webpack-plugin');
+    const WebpackBundleSizeAnalyzerPlugin = require('webpack-bundle-size-analyzer').WebpackBundleSizeAnalyzerPlugin;
+    const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+    const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+    const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+    const ManifestPlugin = require('webpack-manifest-plugin');
+    // var ExtractTextPlugin = require("extract-text-webpack-plugin");// webpack4暂不支持
+    const autoprefixer = require('autoprefixer');
+
+    const path = require('path');
+    const fs = require('fs');
+
+    const appname = userConf.appname; 
+
+    //选择entry目录下的所有文件
+    let opts = (function () {
+        var obj = {}
+        var htmlplugins = [];
+        var files = fs.readdirSync(path.resolve(userConf.dirname, './src/entry'));
+
+        // npm公共模块
+        if (userConf.libs) {
+            obj['libs'] = userConf.libs || '';
+        }
+
+        files.forEach(function (name, index) {
+            var stat = fs.statSync(path.resolve(userConf.dirname, './src/entry', name));
+            if (!stat.isDirectory()) {
+                obj[name.split('.')[0]] = path.resolve(userConf.dirname, './src/entry', name);
+            }
+        })
+        return {
+            entries: obj,
+            htmlplugins: htmlplugins
+        };
+    })();
+    let map = {
+        jsx: {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'babel-loader'
+                }
+            ]
+        },
+        less: {
+            test: /\.less$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        ident: 'postcss',
+                        plugins: [autoprefixer({
+                            browsers: ['last 2 versions', 'Android >= 4.0', 'iOS 7'],
+                        })]
+                    }
+                },
+                {
+                    loader: 'less-loader',
+                },
+            ]
+        },
+        vue: {
+            test: /\.vue$/,
+            use: ["vue-loader"]
+        }
+    }
+    let rules = [];
+    for (var i = 0; i < userConf.supportFileType.length; i++) {
+        if (map[userConf.supportFileType[i]]) {
+            rules.push(map[userConf.supportFileType[i]]);
+        }
+    }
+    
+    return {
+        mode: 'production',// development || production
+        entry: opts.entries,
+        output: {
+            path: userConf.dirname + `/build/static/${appname}`,
+            filename: "[name].min_[chunkhash:8].js",
+        },
+        resolve:{
+            alias:{
+                Src: path.resolve(userConf.dirname, '/src/')
+            }
+        },
+        module: { // 在配置文件里添加JSON loader
+            rules: [
+                {
+                    test: /\.js?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader'
+                        }
+                    ]
+                }, 
+                {
+                    test: /\.json$/,
+                    use: [
+                        {
+                            loader: 'json-loader'
+                        }
+                    ]
+                },
+                 
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: [autoprefixer({
+                                    browsers: ['last 2 versions', 'Android >= 4.0', 'iOS 7'],
+                                })]
+                            }
+                        }
+                    ]
+                }, 
+                
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf)$/,
+                    use: {
+                        loader: 'url-loader',
+                        options:{
+                            limit: 5*1024,
+                            name:'[path][name].[ext]',
+                            outputPath: userConf.dirname + `/build/static/${appname}/img`
+                        }
+                    }
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|eot|woff|woff2|ttf)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options:{
+                            name:'[path][name].[ext]',
+                            outputPath: userConf.dirname + `/build/static/${appname}/img`
+                        }
+                    }
+                }
+
+            ].concat(rules)
+        },
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true
+                }),
+                new OptimizeCSSAssetsPlugin({}),  // use OptimizeCSSAssetsPlugin
+            ],
+            splitChunks: {
+                cacheGroups: {
+                    styles: {            
+                        name: 'styles',
+                        test: /\.less|css$/,
+                        chunks: 'all',    // merge all the css chunk to one file
+                        enforce: true
+                    }
+                }
+            }
+        },
+        plugins: [
+            new ManifestPlugin({
+                writeToFileEmit: true,
+                publicPath: userConf.publicStaticDomain + `/static/${appname}/`
+            }),
+            new CopyWebpackPlugin([
+                // 切记放在new HtmlWebpackPlugin之前
+                {
+                   from: path.resolve(userConf.dirname, './server'),
+                   to: path.resolve(userConf.dirname, `./build/server/${appname}/`),
+                   ignore: ['.*']
+                },
+                {
+                   from: path.resolve(userConf.dirname, './conf'),
+                   to: path.resolve(userConf.dirname, `./build/conf/${appname}/`),
+                   ignore: ['.*']
+                },
+                {
+                   from: path.resolve(userConf.dirname, './src/template'),
+                   to: path.resolve(userConf.dirname, `./build/template/${appname}/`),
+                   ignore: ['.*']
+                },
+                {
+                   from: path.resolve(userConf.dirname, './src/static'),
+                   to: path.resolve(userConf.dirname, `./build/static/${appname}/`),
+                   ignore: ['.*']
+                },
+                {
+                   from: path.resolve(userConf.dirname, './src'),
+                   to: path.resolve(userConf.dirname, `./build/src/${appname}/`),
+                   ignore: ['.*']
+                }
+            ]),
+
+            new MiniCssExtractPlugin({
+                filename: "css/[name].[contenthash:12].css",
+                chunkFilename: "css/[id].[contenthash:12].css"
+            }),
+        ]
+
+    }
+}
